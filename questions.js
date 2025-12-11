08c7e85f9fcd49d478b7c0e8cdcdb092a01d039c
neomediavalist <mikolaus54@proton.me>
2025-12-11 00:44:53 +0000

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,547 @@
-
+questions = [
+    // Authority - Anarchy
+    {
+        "question": "A minimal government is necessary to enforce contracts and protect property rights.",
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Law enforcement should exist to prevent aggression and fraud.",
+        "effect": {
+            "a": 10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Courts are essential to resolve disputes fairly between individuals.",
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "A government monopoly on justice ensures impartial enforcement of laws.",
+        "effect": {
+            "a": 10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Individuals should follow laws that protect others’ freedoms.",
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "An organized legal system is necessary to prevent chaos.",
+        "effect": {
+            "a": 10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Ideally government would not exist.",
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Police and other hierarchical law and justice organizations should be abolished.",
+        "effect": {
+            "a": -10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "People can take care of each other without help or protection from the government.",
+        "effect": {
+            "a": -10,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0
+        }
+    },
+    {
+        "question": "We should abolish taxes.",
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "If state exists, it should be minimal as possible.",
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Law should be less complex, if it exists at all.",
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    // Hierarchy - Heterarchy
+    {
+        "question": "Not everyone should be allowed to make decisions.",
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Communities should be free to establish their own internal hierarchies.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Top down structures for organisations work best.",
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Membership in a community should require consent to its governing authority.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Exit from a community is voluntary, but inside it, authority is binding.",
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Private law enforcement can enforce rules agreed upon by property owners.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Communities should operate without imposed leaders or rulers.",
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "All hierarchies must be questioned.",
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Power should be distributed horizontally rather than concentrated.",
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Co-operatives are worse than traditional business model.",
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Disputes are best resolved through mediation, not centralized authority",
+        "effect": {
+            "a": 0,
+            "b": -5,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Social order can emerge naturally without formal hierarchies or rulers.",
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+            "e": 0
+        }
+    },
+    // Planning - Markets
+    {
+        "question": "We should abolish money.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 10,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Decentrally planned economy is better than market economy.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 10,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Mixed economy is better than fully free market one.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Market economies are inefficient.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 10,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "We should produce based on need and use not commodity.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Economy needs coordination.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "The market will regulate itself.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -10,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "The freerer the market the freerer the people.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -10,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Trade between individuals should be the basis of our economy.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Economic planning will always lead to authoritarianism.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -10,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Mixed economy is better than a fully planned one.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0
+        }
+    },
+    {
+        "question": "Prices should be determined by supply and demand.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0
+        }
+    },
+    // Collectivism - Individualism
+    {
+        "question": "Collective or communal agriculture is better than private or individual one.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0
+        }
+    },
+    {
+        "question": "Society should prioritize the needs of the group over individual gain.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 10,
+            "e": 0
+        }
+    },
+    {
+        "question": "From each according to his ability, to each according to his needs.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 10,
+            "e": 0
+        }
+    },
+    {
+        "question": "Wage labour should be less common, if not fully abolished.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0
+        }
+    },
+    {
+        "question": "Mutual aid and/or welfare is generally better than charity.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0
+        }
+    },
+    {
+        "question": "Inheritance of anything that is not personal possession is illegitimate.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 10,
+            "e": 0
+        }
+    },
+    {
+        "question": "Without property rights there are no rights at all.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -10,
+            "e": 0
+        }
+    },
+    {
+        "question": "Theft can never be justified.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+            "e": 0
+        }
+    },
+    {
+        "question": "People should be free to contract, trade, and cooperate as they choose.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+            "e": 0
+        }
+    },
+    {
+        "question": "If someone owns a piece of land, they should be able to set own rules on it.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -10,
+            "e": 0
+        }
+    },
+    {
+        "question": "Voluntary agreements are preferable to collective mandates.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -10,
+            "e": 0
+        }
+    },
+    {
+        "question": "Individuals have the right to control their own property and resources.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": -5,
+            "e": 0
+        }
+    },
+    // Horun - Verun
+    {
+        "question": "Libertarians should cooperate across ideological divides to defend freedom.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 10
+        }
+    },
+    {
+        "question": "Ideological purity is less important than practical cooperation among libertarians.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 10
+        }
+    },
+    {
+        "question": "Cooperation between left-libertarians and right-libertarians strengthens liberty.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 10
+        }
+    },
+    {
+        "question": "I prefer Left-Unity/Right-Unity over Lib-Unity.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": -10
+        }
+    },
+    {
+        "question": "Libertarian Socialists and Libertarian Capitalists are too different to work toghether.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": -10
+        }
+    },
+    {
+        "question": "Supporting stronger authoritarian factions aligned with your politics is sometimes necessary.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": -10
+        }
+    }
+];
