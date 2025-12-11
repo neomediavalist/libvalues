d62e5650d0124af769614f320ba05a0426849a29
neomediavalist <mikolaus54@proton.me>
2025-12-11 00:44:36 +0000

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,1 +1,123 @@
-
+const ideologies = {
+  // libright
+  'Hoppeanism': {
+    a: 37,
+    b: 58,
+    c: 7,
+    d: 15,
+    e: 8
+  },
+  'Libertarian-Monarchism': {
+    a: 70,
+    b: 80,
+    c: 7,
+    d: 7,
+    e: 5
+  },
+  'Minarchism': {
+    a: 64,
+    b: 76,
+    c: 14,
+    d: 14,
+    e: 75
+  },
+  'Paleolibertarianism': {
+    a: 60,
+    b: 65,
+    c: 7,
+    d: 7,
+    e: 3
+  },
+  'Anarcho-Capitalism': {
+    a: 10,
+    b: 20,
+    c: 7,
+    d: 7,
+    e: 50
+  },
+  'Avaritionism': {
+    a: 0,
+    b: 35,
+    c: 0,
+    d: 0,
+    e: 20
+  },
+  'Bleeding-Heart': {
+    a: 57,
+    b: 54,
+    c: 17,
+    d: 24,
+    e: 90
+  },
+  'NRx': {
+    a: 60,
+    b: 100,
+    c: 30,
+    d: 15,
+    e: 30
+  },
+  'Hayekism': {
+    a: 54,
+    b: 51,
+    c: 8,
+    d: 22,
+    e: 75
+  },
+  // libleft
+  'Anarcho-Communism': {
+    a: 0,
+    b: 0,
+    c: 80,
+    d: 90,
+    e: 50
+  },
+  'Autonomism': {
+    a: 0,
+    b: 10,
+    c: 50,
+    d: 80,
+    e: 50
+  },
+  'Democratic-Confederalism': {
+    a: 7,
+    b: 10,
+    c: 40,
+    d: 70,
+    e: 70
+  },
+  'Ego-Communism': {
+    a: 0,
+    b: 20,
+    c: 70,
+    d: 40,
+    e: 70
+  },
+  'Anarcho-Mutualism': {
+    a: 0,
+    b: 10,
+    c: 30,
+    d: 60,
+    e: 50
+  },
+  'Minarcho-Mutualism': {
+    a: 60,
+    b: 10,
+    c: 30,
+    d: 60,
+    e: 50
+  },
+  'Minarcho-Socialism': {
+    a: 50,
+    b: 20,
+    c: 60,
+    d: 80,
+    e: 30
+  },
+  'Platformism': {
+    a: 0,
+    b: 70,
+    c: 80,
+    d: 90,
+    e: 60
+  }
+}
