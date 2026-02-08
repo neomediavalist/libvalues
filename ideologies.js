96848ec64524ca86d04e0e0716f0643702d154ea
neomediavalist <mikolaus54@proton.me>
2026-02-08 22:33:18 +0000

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,54 +1,29 @@
 const ideologies = {
   // libright
+
+  // Anarchists
+
   'Hoppeanism': {
-    a: 40,
+    a: 20,
     b: 58,
     c: 0,
     d: 0,
     e: 8,
     f: 50,
     g: 70,
-    h: 0
-  },
-  'Libertarian-Monarchism': {
-    a: 70,
-    b: 80,
-    c: 0,
-    d: 0,
-    e: 5,
-    f: 50,
-    g: 60,
-    h: 40
-  },
-  'Minarchism': {
-    a: 64,
-    b: 76,
-    c: 10,
-    d: 10,
-    e: 75,
-    f: 50,
-    g: 50,
-    h: 45
-  },
-  'Paleolibertarianism': {
-    a: 60,
-    b: 65,
-    c: 0,
-    d: 0,
-    e: 3,
-    f: 50,
-    g: 70,
-    h: 0
+    h: 0,
+    i: 100
   },
   'Anarcho-Capitalism': {
-    a: 10,
+    a: 0,
     b: 20,
     c: 0,
     d: 10,
     e: 85,
     f: 40,
     g: 10,
-    h: 10
+    h: 10,
+    i: 100
   },
   'Avaritionism': {
     a: 0,
@@ -58,107 +33,19 @@ const ideologies = {
     e: 20,
     f: 20,
     g: 50,
-    h: 60
-  },
-  'Bleeding-Heart': {
-    a: 57,
-    b: 54,
-    c: 17,
-    d: 24,
-    e: 90,
-    f: 60,
-    g: 30,
-    h: 20
+    h: 60,
+    i: 100
   },
   'NRx': {
-    a: 50,
+    a: 30,
     b: 100,
     c: 0,
     d: 20,
     e: 0,
     f: 0,
     g: 100,
-    h: 65
-  },
-  'Hayekism': {
-    a: 50,
-    b: 40,
-    c: 30,
-    d: 30,
-    e: 80,
-    f: 35,
-    g: 40,
-    h: 40
-  },
-  'ClassLib': {
-    a: 30,
-    b: 40,
-    c: 20,
-    d: 20,
-    e: 70,
-    f: 40,
-    g: 55,
-    h: 30
-  },
-  'NeoClassLib': {
-    a: 35,
-    b: 45,
-    c: 22,
-    d: 30,
-    e: 70,
-    f: 30,
-    g: 50,
-    h: 55
-  },
-  'Neolibertarianism': {
-    a: 45,
-    b: 50,
-    c: 35,
-    d: 35,
-    e: 30,
-    f: 30,
-    g: 65,
-    h: 100
-  },
-  'Fusionism': {
-    a: 55,
-    b: 50,
-    c: 35,
-    d: 35,
-    e: 50,
-    f: 30,
-    g: 75,
-    h: 80
-  },
-  'Neoliberalism': {
-    a: 50,
-    b: 50,
-    c: 40,
-    d: 40,
-    e: 40,
-    f: 30,
-    g: 30,
-    h: 70
-  },
-  'Paleoconservatism': {
-    a: 65,
-    b: 70,
-    c: 30,
-    d: 30,
-    e: 10,
-    f: 50,
-    g: 90,
-    h: 0
-  },
-  'Ordoliberalism': {
-    a: 50,
-    b: 50,
-    c: 50,
-    d: 50,
-    e: 50,
-    f: 40,
-    g: 50,
-    h: 50
+    h: 65,
+    i: 100
   },
   'Agorism': {
     a: 0,
@@ -168,19 +55,221 @@ const ideologies = {
     e: 100,
     f: 30,
     g: 30,
-    h: 0
+    h: 0,
+    i: 100
   },
-  'Alt-Right': {
+
+  // Minarchists
+
+  'Minarchism': {
+    a: 70,
+    b: 50,
+    c: 10,
+    d: 10,
+    e: 75,
+    f: 50,
+    g: 50,
+    h: 30,
+    i: 70
+  },
+  'Libertarian-Monarchism': {
+    a: 70,
+    b: 80,
+    c: 0,
+    d: 0,
+    e: 5,
+    f: 50,
+    g: 60,
+    h: 40,
+    i: 100
+  },
+  'Paleolibertarianism': {
+    a: 60,
+    b: 65,
+    c: 0,
+    d: 0,
+    e: 3,
+    f: 50,
+    g: 70,
+    h: 0,
+    i: 100
+  },
+  'Bleeding-Heart': {
+    a: 60,
+    b: 50,
+    c: 20,
+    d: 30,
+    e: 90,
+    f: 70,
+    g: 0,
+    h: 0,
+    i: 60
+  },
+  'Hayekism': {
+    a: 50,
+    b: 40,
+    c: 0,
+    d: 30,
+    e: 80,
+    f: 40,
+    g: 40,
+    h: 10,
+    i: 50
+  },
+  'Fusionism': {
+    a: 60,
+    b: 65,
+    c: 0,
+    d: 15,
+    e: 20,
+    f: 50,
+    g: 70,
+    h: 70,
+    i: 70
+  },
+  'Geolibertarianism': {
+    a: 30,
+    b: 40,
+    c: 20,
+    d: 25,
+    e: 70,
+    f: 50,
+    g: 40,
+    h: 0,
+    i: 20
+  },
+  'Neolibertarianism': {
+    a: 30,
+    b: 50,
+    c: 0,
+    d: 25,
+    e: 40,
+    f: 30,
+    g: 36,
+    h: 100,
+    i: 60
+  },
+  'Geoneolibertarianism': {
+    a: 30,
+    b: 50,
+    c: 0,
+    d: 25,
+    e: 40,
+    f: 30,
+    g: 36,
+    h: 100,
+    i: 20
+  },
+  // Liberals
+
+  'ClassLib': {
+    a: 50,
+    b: 60,
+    c: 0,
+    d: 0,
+    e: 90,
+    f: 50,
+    g: 55,
+    h: 30,
+    i: 60
+  },
+  'GeoclassLib': {
+    a: 50,
+    b: 60,
+    c: 0,
+    d: 0,
+    e: 90,
+    f: 50,
+    g: 55,
+    h: 30,
+    i: 20
+  },
+  'ManchesterLib': {
+    a: 40,
+    b: 40,
+    c: 0,
+    d: 0,
+    e: 90,
+    f: 50,
+    g: 55,
+    h: 0,
+    i: 60
+  },
+  'Neoliberalism': {
     a: 80,
-    b: 100,
-    c: 40,
-    d: 50,
+    b: 60,
+    c: 0,
+    d: 25,
+    e: 40,
+    f: 30,
+    g: 35,
+    h: 100,
+    i: 60
+  },
+  'Geoneoliberalism': {
+    a: 80,
+    b: 60,
+    c: 0,
+    d: 25,
+    e: 40,
+    f: 30,
+    g: 35,
+    h: 100,
+    i: 20
+  },
+  'Ordoliberalism': {
+    a: 80,
+    b: 60,
+    c: 35,
+    d: 35,
+    e: 50,
+    f: 30,
+    g: 35,
+    h: 50,
+    i: 60
+  },
+
+  // conservatives
+
+  'Paleoconservatism': {
+    a: 100,
+    b: 80,
+    c: 10,
+    d: 30,
     e: 0,
     f: 50,
-    g: 80,
-    h: 80
+    g: 90,
+    h: 0,
+    i: 80
   },
+  'Geoconservatism': {
+    a: 80,
+    b: 60,
+    c: 10,
+    d: 30,
+    e: 0,
+    f: 60,
+    g: 90,
+    h: 0,
+    i: 20
+  },
+  'Conservatarianism': {
+    a: 50,
+    b: 60,
+    c: 10,
+    d: 30,
+    e: 0,
+    f: 50,
+    g: 90,
+    h: 0,
+    i: 80
+  },
+
+
   // libleft
+
+  // Anarchists
+
   'Anarcho-Communism': {
     a: 0,
     b: 0,
@@ -189,7 +278,19 @@ const ideologies = {
     e: 50,
     f: 60,
     g: 0,
-    h: 0
+    h: 0,
+    i: 0
+  },
+  'Anarcho-Syndicalism': {
+    a: 0,
+    b: 0,
+    c: 75,
+    d: 100,
+    e: 50,
+    f: 30,
+    g: 0,
+    h: 0,
+    i: 0
   },
   'Autonomism': {
     a: 0,
@@ -199,7 +300,8 @@ const ideologies = {
     e: 50,
     f: 50,
     g: 0,
-    h: 10
+    h: 10,
+    i: 0
   },
   'Democratic-Confederalism': {
     a: 0,
@@ -209,7 +311,8 @@ const ideologies = {
     e: 70,
     f: 60,
     g: 20,
-    h: 80
+    h: 80,
+    i: 0
   },
   'Ego-Communism': {
     a: 0,
@@ -219,37 +322,8 @@ const ideologies = {
     e: 70,
     f: 40,
     g: 10,
-    h: 0
-  },
-  'Anarcho-Mutualism': {
-    a: 0,
-    b: 0,
-    c: 15,
-    d: 60,
-    e: 50,
-    f: 50,
-    g: 20,
-    h: 0
-  },
-  'Minarcho-Mutualism': {
-    a: 70,
-    b: 10,
-    c: 20,
-    d: 75,
-    e: 50,
-    f: 40,
-    g: 30,
-    h: 50
-  },
-  'Minarcho-Socialism': {
-    a: 50,
-    b: 20,
-    c: 60,
-    d: 80,
-    e: 30,
-    f: 30,
-    g: 20,
-    h: 30
+    h: 0,
+    i: 30
   },
   'Platformism': {
     a: 0,
@@ -259,78 +333,154 @@ const ideologies = {
     e: 60,
     f: 50,
     g: 35,
-    h: 0
+    h: 0,
+    i: 0
   },
-  'LeftRoth': {
-    a: 0,
-    b: 40,
-    c: 0,
-    d: 40,
-    e: 100,
-    f: 45,
-    g: 40,
-    h: 0
-  },
-  'Geolibertarianism': {
-    a: 30,
-    b: 30,
-    c: 30,
-    d: 40,
-    e: 70,
-    f: 65,
-    g: 40,
-    h: 40
-  },
-  'Green-Libertarianism': {
-    a: 50,
-    b: 20,
-    c: 30,
-    d: 50,
-    e: 80,
-    f: 90,
-    g: 20,
-    h: 0
-  },
-  'Anarcho-Primitivism': {
+  'Soulism': {
     a: 0,
     b: 0,
-    c: 80,
-    d: 60,
-    e: 80,
-    f: 100,
-    g: 35,
-    h: 0
+    c: 100,
+    d: 70,
+    e: 100,
+    f: 0,
+    g: 0,
+    h: 50,
+    i: 0
+  },
+
+  // Minarchists
+
+  'Minarcho-Socialism': {
+    a: 50,
+    b: 20,
+    c: 60,
+    d: 80,
+    e: 30,
+    f: 30,
+    g: 20,
+    h: 30,
+    i: 0
   },
   'Guild-Socialism': {
     a: 40,
     b: 30,
-    c: 70,
+    c: 60,
     d: 80,
     e: 50,
     f: 50,
-    g: 35,
-    h: 20
+    g: 30,
+    h: 0,
+    i: 30
   },
-  'Green-Anarchism': {
-    a: 0,
-    b: 0,
-    c: 80,
-    d: 60,
+  'Market-Socialism': {
+    a: 40,
+    b: 20,
+    c: 30,
+    d: 90,
+    e: 60,
+    f: 40,
+    g: 20,
+    h: 0,
+    i: 30
+  },
+  'Geosyndicalism': {
+    a: 70,
+    b: 40,
+    c: 70,
+    d: 90,
     e: 80,
-    f: 90,
+    f: 20,
     g: 10,
-    h: 0
+    h: 0,
+    i: 0
   },
-  // central
+  'Social-Georgism': {
+    a: 80,
+    b: 50,
+    c: 30,
+    d: 60,
+    e: 90,
+    f: 60,
+    g: 20,
+    h: 0,
+    i: 0
+  },
+
+  // liberals
+
+  'Social-Democrat': {
+    a: 80,
+    b: 50,
+    c: 30,
+    d: 60,
+    e: 90,
+    f: 60,
+    g: 20,
+    h: 0,
+    i: 60
+  },
+  'Social-Liberal': {
+    a: 70,
+    b: 50,
+    c: 20,
+    d: 50,
+    e: 100,
+    f: 60,
+    g: 20,
+    h: 30,
+    i: 70
+  },
+  'Liberal-Socialist': {
+    a: 70,
+    b: 50,
+    c: 40,
+    d: 70,
+    e: 80,
+    f: 60,
+    g: 20,
+    h: 10,
+    i: 40
+  },
+
+  // conservatives
+
+  'Agrarian-Socialist': {
+    a: 70,
+    b: 20,
+    c: 80,
+    d: 100,
+    e: 40,
+    f: 100,
+    g: 70,
+    h: 0,
+    i: 0
+  },
+  'Distributist-Socialist': {
+    a: 80,
+    b: 20,
+    c: 70,
+    d: 80,
+    e: 50,
+    f: 100,
+    g: 90,
+    h: 20,
+    i: 10
+  },
+
+  // center
+
+  // Anarchists
+
   'Anarcho-Egoism': {
     a: 0,
     b: 0,
-    c: 30,
-    d: 0,
+    c: 20,
+    d: 30,
     e: 100,
     f: 50,
     g: 0,
-    h: 0
+    h: 0,
+    i: 40
   },
   'Religious-Anarchism': {
     a: 0,
@@ -338,8 +488,161 @@ const ideologies = {
     c: 50,
     d: 60,
     e: 80,
-    f: 70,
+    f: 80,
     g: 100,
-    h: 0
+    h: 0,
+    i: 10
+  },
+  'LeftRoth': {
+    a: 0,
+    b: 50,
+    c: 0,
+    d: 40,
+    e: 100,
+    f: 30,
+    g: 50,
+    h: 0,
+    i: 100
+  },
+  'Anarcho-Primitivism': {
+    a: 0,
+    b: 0,
+    c: 40,
+    d: 70,
+    e: 80,
+    f: 100,
+    g: 70,
+    h: 0,
+    i: 0
+  },
+  'Anarcho-Mutualism': {
+    a: 0,
+    b: 0,
+    c: 15,
+    d: 80,
+    e: 50,
+    f: 50,
+    g: 20,
+    h: 0,
+    i: 0
+  },
+  'Green-Anarchism': {
+    a: 0,
+    b: 0,
+    c: 50,
+    d: 60,
+    e: 80,
+    f: 100,
+    g: 10,
+    h: 0,
+    i: 20
+  },
+  'Geoanarchism': {
+    a: 0,
+    b: 0,
+    c: 20,
+    d: 40,
+    e: 100,
+    f: 50,
+    g: 30,
+    h: 0,
+    i: 0
+  },
+
+  // Minarchists
+
+  'Green-Libertarianism': {
+    a: 60,
+    b: 20,
+    c: 30,
+    d: 50,
+    e: 80,
+    f: 100,
+    g: 20,
+    h: 0,
+    i: 40
+  },
+  'Minarcho-Mutualism': {
+    a: 60,
+    b: 20,
+    c: 15,
+    d: 80,
+    e: 50,
+    f: 50,
+    g: 20,
+    h: 0,
+    i: 0
+  },
+  'Georgism': {
+    a: 50,
+    b: 30,
+    c: 20,
+    d: 40,
+    e: 100,
+    f: 50,
+    g: 30,
+    h: 0,
+    i: 0
+  },
+
+  // Liberals
+
+  'Progressive-Liberal': {
+    a: 60,
+    b: 20,
+    c: 30,
+    d: 40,
+    e: 100,
+    f: 80,
+    g: 0,
+    h: 0,
+    i: 60
+  },
+  'Moderatism': {
+    a: 50,
+    b: 50,
+    c: 50,
+    d: 50,
+    e: 50,
+    f: 50,
+    g: 50,
+    h: 50,
+    i: 50
+  },
+  'Nordic-Model': {
+    a: 60,
+    b: 30,
+    c: 10,
+    d: 60,
+    e: 100,
+    f: 70,
+    g: 10,
+    h: 20,
+    i: 65
+  },
+  'Green-Liberal': {
+    a: 60,
+    b: 30,
+    c: 10,
+    d: 60,
+    e: 100,
+    f: 100,
+    g: 10,
+    h: 20,
+    i: 65
+  },
+
+  // conservatives
+
+  'Christian-Democracy': {
+    a: 70,
+    b: 40,
+    c: 20,
+    d: 60,
+    e: 80,
+    f: 60,
+    g: 80,
+    h: 20,
+    i: 70
   }
 }
